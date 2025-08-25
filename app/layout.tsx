8159402056f9a2af02fe6353dbab7124e58dcf56
Derrick Ho <derrickho@Derricks-MacBook-Pro.local>
2025-08-25 11:08:26 -0700

    updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'deho.design',
   description: "derrick's design portfolio",
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
